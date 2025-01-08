f5f3e4f5d45bf4ed1d824414ba3a02a3295ff70b
elarreglador <elarreglador@protonmail.com>
2025-01-08 19:06:28 +0100

    🎨 Mejora estetica: Aplicado tema de color

--- app/src/main/java/com/elarreglador/ud3_01_room/ui/theme/Color.kt
+++ app/src/main/java/com/elarreglador/ud3_01_room/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Arcilla = Color(0xFF8d533b)
+val Topacio = Color(0xFFbc8f63)
+val Musgo = Color(0xFFa3a182)
+val Helecho = Color(0xFF6e684a)
+val Marfil = Color(0xFFfafbf8)
+val Lino = Color(0xFFf9f0d4)
+val Cava = Color(0xFFebcea3)
+val Betun = Color(0xFF2f2920)
+
--- app/src/main/java/com/elarreglador/ud3_01_room/ui/theme/Theme.kt
+++ app/src/main/java/com/elarreglador/ud3_01_room/ui/theme/Theme.kt
@@ -12,15 +12,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Topacio,
+    onPrimary = Betun,
+    secondary = Helecho,
+    tertiary = Musgo,
+    background = Betun,
+    onBackground = Marfil
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Arcilla,
+    onPrimary = Marfil,
+    secondary = Helecho,
+    tertiary = Musgo,
+    background = Cava,
+    onBackground = Betun
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
