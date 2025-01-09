262b9a60fc30a88893f15bc7a5d8a4f2fff1ef43
elarreglador <elarreglador@protonmail.com>
2025-01-09 16:08:13 +0100

    🧹 Limpieza de codigo

--- app/src/main/java/com/elarreglador/ud3_01_room/screens/BookListViewScreen.kt
+++ app/src/main/java/com/elarreglador/ud3_01_room/screens/BookListViewScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,7 +20,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -29,7 +27,6 @@ import com.elarreglador.ud3_01_room.R
 
 @Composable
 fun BookListViewScreen(navController: NavController) {
-    val context = LocalContext.current
 
     Scaffold(
         topBar = {
@@ -79,13 +76,8 @@ fun BookListViewScreen(navController: NavController) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.padding(16.dp)
             ) {
-
                 Spacer(modifier = Modifier.height(32.dp))
-
-
-
             }
-
         }
     )
 }
