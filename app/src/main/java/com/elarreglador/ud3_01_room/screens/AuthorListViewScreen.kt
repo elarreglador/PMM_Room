0680dcbc2ea57f25d6b6ad7cbb7fa5baebee214b
elarreglador <elarreglador@protonmail.com>
2025-01-10 20:16:25 +0100

    🚀 Cambio mayor: Se muestra el listado de autores en AuthorListViewScreen

--- app/src/main/java/com/elarreglador/ud3_01_room/screens/AuthorListViewScreen.kt
+++ app/src/main/java/com/elarreglador/ud3_01_room/screens/AuthorListViewScreen.kt
@@ -2,12 +2,18 @@ package com.elarreglador.ud3_01_room.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
@@ -18,22 +24,43 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.elarreglador.ud3_01_room.R
+import androidx.compose.runtime.remember
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalContext
+import com.elarreglador.ud3_01_room.database.Author
+import com.elarreglador.ud3_01_room.database.MyDatabase
+
+
 
 @Composable
 fun AuthorListViewScreen(navController: NavController) {
+
+    // Estado para almacenar la lista de autores
+    val authorsState = remember { mutableStateOf<List<Author>>(emptyList()) }
+    // Recuperar los autores al iniciar la pantalla
+    val context = LocalContext.current
+    LaunchedEffect(Unit) {
+        // Mover la obtención de la base de datos fuera del cuerpo composable
+        val db = MyDatabase.getDatabase(context)
+        val authors = db.authorDao().getAllAuthors()
+        authorsState.value = authors
+    }
 
     Scaffold(
         topBar = {
             Row (
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background( MaterialTheme.colorScheme.primary ) // Fondo de color
+                    .background(MaterialTheme.colorScheme.primary) // Fondo de color
                     .padding(25.dp), // Nos separa de la topBar
 
                 verticalAlignment = Alignment.CenterVertically // Alinea verticalmente al centro
@@ -60,7 +87,12 @@ fun AuthorListViewScreen(navController: NavController) {
         floatingActionButton = {
             FloatingActionButton(
                 onClick = { navController.navigate("AuthorAddScreen") },
-                containerColor = MaterialTheme.colorScheme.primary
+                containerColor = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier
+                    .shadow(
+                        elevation = 12.dp,  // Sombra más pronunciada
+                        shape = MaterialTheme.shapes.small,
+                    )
             ) {
                 Icon(
                     imageVector = Icons.Default.Add,
@@ -72,14 +104,63 @@ fun AuthorListViewScreen(navController: NavController) {
         },
         content = { paddingValues ->
             // Contenido principal, respetando los paddings del Scaffold
-            Column (
+            Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .padding(paddingValues) // Aplica el padding del Scaffold
-                    .padding(16.dp) // Agrega un padding adicional si es necesario
+                    .padding(paddingValues)
+                    .padding(16.dp)
             ) {
-                Spacer(modifier = Modifier.height(32.dp))
+                if (authorsState.value.isEmpty()) {
+                    Text("No hay autores disponibles.")
+                } else {
+                    LazyColumn {
+                        items(authorsState.value.size) { index ->
+
+                            Row (
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            ){
+                                Box( // box para la id del autor
+                                    modifier = Modifier
+                                        .background(MaterialTheme.colorScheme.secondary)
+                                        .widthIn(min = 60.dp) // Establece un ancho mínimo
+                                        .fillMaxHeight()
+                                ) {
+                                    val author = authorsState.value[index]
+                                    Text(
+                                        text = "${author.id}",
+                                        color = MaterialTheme.colorScheme.onSecondary,
+                                        modifier = Modifier.align(Alignment.Center)
+                                    )
+                                }
+
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(
+                                            color = MaterialTheme.colorScheme.primary,
+                                        )
+                                        .padding(6.dp)
+                                        .fillMaxHeight()
+                                ) {
+                                    val author = authorsState.value[index]
+                                    Text(
+                                        text = "${author.name} ${author.surname} \n${author.country}",
+                                        color = MaterialTheme.colorScheme.onPrimary,
+                                    )
+                                }
+                            }
+
+                            Spacer(
+                                modifier = Modifier
+                                    .height(8.dp)
+                            )
+                        }
+                    }
+                }
             }
         }
     )
 }
+
+
